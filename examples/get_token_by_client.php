5af17a2fe6d411196554e52cbb361d12f6cf638b
xiongfusong <xiongfusong@gmail.com>
2015-11-23 20:08:28 +0800

    example

--- examples/get_token_by_client.php
+++ examples/get_token_by_client.php
@@ -3,13 +3,14 @@ require '../src/Oauth2_client/Autoloader.php';
 Oauth2_client\Autoloader::register();
 use Oauth2_client\Token;
 
-$key = 'client_token';
-$client = new Token(array(
-    'cache_token_key'=> $key,
-    'client_id'=> 'demoapp',
-    'client_secret'=> 'demopass',
-    'auth_url'=> 'http://api67.pigai.org/oauth2/access_token'
-));
+$config = array(
+    'cache_token_key'=> 'your_token_cache_name',
+    'client_id'=> 'your_app_key',
+    'client_secret'=> 'your_app_secret',
+    'auth_url'=> 'http://api.pigai.org/oauth2/access_token'
+)
+
+$client = new Token($config));
 $access_token = $client->getValidAccessToken('client_credentials');
 
 var_dump($access_token);
